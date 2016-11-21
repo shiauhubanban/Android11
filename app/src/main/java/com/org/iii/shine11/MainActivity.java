ec23436364a449a58d3edd4974d9bdcc65a9d68b
user <a>
2016-11-21 15:12:09 +0800

    v3

--- app/src/main/java/com/org/iii/shine11/MainActivity.java
+++ app/src/main/java/com/org/iii/shine11/MainActivity.java
@@ -1,13 +1,37 @@
 package com.org.iii.shine11;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private MyDBHelper dbHelper;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.tv);
+
+        dbHelper = new MyDBHelper(this,"iii",null,1);
+        db = dbHelper.getReadableDatabase();
+    }
+    public void query(View v){
+        textView.setText("");
+        // SELECT * FROM cust
+        // db.execSQL("SELECT * FROM cust");
+        Cursor cursor = db.query("cust",null,null,null,null,null,null);
+        while (cursor.moveToNext()){
+            String id = cursor.getString(0);
+            String cname = cursor.getString(1);
+            String birthday = cursor.getString(2);
+            String tel = cursor.getString(3);
+            textView.setText(id +":"+ cname + ":" + birthday + ":" + tel + "\n");
+        }
     }
 }
